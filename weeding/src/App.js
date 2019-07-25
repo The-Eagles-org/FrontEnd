a2416a839b57f05c35338d063f9acfbe724e782d
Khalotis11 <moekhaloti@gmail.com>
2019-07-26 00:49:33 +0100

    Added basic register component

--- weeding/src/App.js
+++ weeding/src/App.js
@@ -1,10 +1,13 @@
 import React from "react";
 import "./App.css";
 
+import { Register } from "./components/Register"
+
 function App() {
   return (
     <div className="App">
       <h2>Wedding planner</h2>
+      <Register />
     </div>
   );
 }
---
+++ weeding/src/components/Register.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const Register = () => {
+  return (
+    <div className="register-container">
+      <h3>Register</h3>
+    </div>
+  );
+};
